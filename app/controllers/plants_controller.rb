d3bafc1c712667142021c16f27d244ce6c3681f6
Peter <peterkilungya1@gmail.com>
2022-12-01 10:26:52 +0300

    assignment completed

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -1,21 +1,39 @@
 class PlantsController < ApplicationController
-
   # GET /plants
   def index
     plants = Plant.all
     render json: plants
   end
-
   # GET /plants/:id
   def show
     plant = Plant.find_by(id: params[:id])
     render json: plant
   end
-
   # POST /plants
   def create
     plant = Plant.create(plant_params)
     render json: plant, status: :created
+  end
+
+  def update
+    plant=Plant.find_by(id:params[:id])
+    if plant
+      plant.update(plant_params)
+      render json: plant
+    else
+      render json: {error: "Plant not found"}, status: :not_found
+    end
+  end
+
+  def destroy
+    plant=Plant.find_by(id:params[:id])
+    if plant
+      plant.destroy
+      head :no_content
+    else
+      render json: {error: "Plant not found"}, status: :not_found
+    end
+
   end
 
   private
